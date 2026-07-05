81cc610bce073aa65cb24d65691d39ad1ab0fa53
Madhura <madhurasrisenthilkumar@gmail.com>
2026-07-05 09:41:27 +0530

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar() {
+  return (
+    <nav>
+      <div className="logo">Freelancer Marketplace</div>
+
+      <ul>
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#">My Profile</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Freelancers</a></li>
+        <li><a href="#">Milestone Payments</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Logout</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
